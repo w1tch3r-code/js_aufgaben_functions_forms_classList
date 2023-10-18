3f0b1244164062934e01f6e72d0399f01e42f2ac
w1tch3r-code <rainerbrain@gmail.com>
2023-10-18 19:39:31 +0200

    2. Variante bei Level 3_1 erstellt

--- functions-level-3_1/assets/js/main.js
+++ functions-level-3_1/assets/js/main.js
@@ -9,30 +9,36 @@ console.log("%c JS-Einführung – Functions-Level-3_1", "color: tomato");
 const body = document.body;
 const headline = document.querySelector('.headline');
 
-function changeColor1() {
-    headline.classList.remove(...headline.classList)
-    body.classList.remove(...body.classList);
-    headline.classList.add('text__color1')
-    body.classList.add('background__color1');
+function changeColor(textColor, backgroundColor) {
+    headline.style.color = textColor;
+    body.style.backgroundColor = backgroundColor;
 }
 
-function changeColor2() {
-    headline.classList.remove(...headline.classList)
-    body.classList.remove(...body.classList);
-    headline.classList.add('text__color2')
-    body.classList.add('background__color2');
-}
 
-function changeColor3() {
-    headline.classList.remove(...headline.classList)
-    body.classList.remove(...body.classList);
-    headline.classList.add('text__color3')
-    body.classList.add('background__color3');
-}
+// function changeColor1() {
+//     headline.classList.remove(...headline.classList)
+//     body.classList.remove(...body.classList);
+//     headline.classList.add('text__color1')
+//     body.classList.add('background__color1');
+// }
 
-function changeColor4() {
-    headline.classList.remove(...headline.classList)
-    body.classList.remove(...body.classList);
-    headline.classList.add('text__color4')
-    body.classList.add('background__color4');
-}
+// function changeColor2() {
+//     headline.classList.remove(...headline.classList)
+//     body.classList.remove(...body.classList);
+//     headline.classList.add('text__color2')
+//     body.classList.add('background__color2');
+// }
+
+// function changeColor3() {
+//     headline.classList.remove(...headline.classList)
+//     body.classList.remove(...body.classList);
+//     headline.classList.add('text__color3')
+//     body.classList.add('background__color3');
+// }
+
+// function changeColor4() {
+//     headline.classList.remove(...headline.classList)
+//     body.classList.remove(...body.classList);
+//     headline.classList.add('text__color4')
+//     body.classList.add('background__color4');
+// }
